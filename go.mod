5ae429b9efced02ab2e1c7bef41b95847f00b12e
Adin Schmahmann <adin.schmahmann@gmail.com>
2022-02-22 23:06:19 -0500

    add git handler

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/aschmahmann/dnslink-server
 
 go 1.16
 
-require github.com/miekg/dns v1.1.42
+require (
+	github.com/ipfs/go-cid v0.0.7
+	github.com/miekg/dns v1.1.42
+	github.com/multiformats/go-multihash v0.0.13
+)
--- main.go
+++ main.go
@@ -3,70 +3,134 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os/exec"
-	"sort"
 	"strings"
 
 	"github.com/miekg/dns"
+
+	"github.com/ipfs/go-cid"
+	"github.com/multiformats/go-multihash"
 )
 
-func main()  {
-	s := dns.NewServeMux()
-	s.Handle("git", &GitHandler{})
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		body, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			return
+		}
+		reqm := &dns.Msg{}
+		if err := reqm.Unpack(body); err != nil {
+			return
+		}
+
+		writer.Header().Set("Content-Type", dohMimeType)
+		if err := writeGit(writer, reqm); err != nil {
+			return
+		}
+	})
+
+	err := http.ListenAndServeTLS(":3333", "server.crt", "server.key", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
-type GitHandler struct {}
+const (
+	dohMimeType = "application/dns-message"
+)
 
-func (g GitHandler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+func writeGit(w io.Writer, r *dns.Msg) error {
 	n := r.Question[0].Name
 	t := r.Question[0].Qtype
 
 	if t != dns.TypeTXT {
-		_ = w.Close()
-		return
+		return errors.New("only TXT records are supported")
 	}
 
+	// Expected format is head.repobase-repopath.git which will become https://repo
+	// where any - in the name must be replaced with --.
+	// Not yet supported: characters valid in path but not domain name, http (not https)
 	parts := strings.Split(n, ".")
-	if len(parts) == 1 {
-		_ = w.Close()
-		return
+	if len(parts) < 4 {
+		return errors.New("needs more at least 4 domain parts")
 	}
 
 	head := parts[0]
-	parts = parts[1:]
+	parts = parts[1 : len(parts)-2]
+	repo := "https://"
+	var repobase, repopath string
+	for i, p := range parts {
+		if p == "-" {
+			repobase = strings.Join(parts[i+1:], ".")
+			reverse(parts[:i])
+			repopath = strings.Join(parts[:i], "/")
+			repo += repobase + "/" + repopath
+			break
+		}
+	}
+	if repobase == "" {
+		repo = strings.Join(parts, ".")
+	}
 
-	parts[len(parts) - 1] = "http:/"
-	_ = sort.Reverse(sort.StringSlice(parts))
-	repo := strings.Join(parts,"/")
-
-	res, err  := exec.Command("git", "ls-remote", repo).Output()
+	res, err := exec.Command("git", "ls-remote", repo).Output()
 	if err != nil {
-		_ = w.Close()
-		return
+		return err
 	}
 
 	scan := bufio.NewScanner(bytes.NewReader(res))
 	for scan.Scan() {
 		txt := scan.Text()
 		if strings.Contains(txt, fmt.Sprintf("refs/heads/%s", head)) {
-			gitHash := txt[0:strings.Index(txt," ")]
+			gitHash := txt[0:strings.Index(txt, "\t")]
+			b, err := hex.DecodeString(gitHash)
+			if err != nil {
+				return err
+			}
+			mh, err := multihash.Encode(b, multihash.SHA1)
+			if err != nil {
+				return err
+			}
+			c := cid.NewCidV1(cid.GitRaw, mh)
 
-			_ = w.WriteMsg(&dns.Msg{Answer: []dns.RR{&dns.TXT{
-				Hdr: dns.RR_Header{
-					Ttl:      5, // seconds
-				},
-				Txt: []string{gitHash},
-			}}})
-			return
+			msg := new(dns.Msg)
+			msg.SetReply(r)
+
+			t := &dns.TXT{
+				Hdr: dns.RR_Header{Name: "localhost.", Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: 5},
+				Txt: []string{"dnslink=/ipfs/" + c.String()},
+			}
+
+			msg.Answer = append(msg.Answer, t)
+
+			msgb, err := msg.Pack()
+			if err != nil {
+				return err
+			}
+
+			_, err = w.Write(msgb)
+			if err != nil {
+				return err
+			}
+			return nil
 		}
 	}
 
 	if err := scan.Err(); err != nil {
 		// Handle the error
-		_ = w.Close()
-		return
+		return err
 	}
+	return nil
 }
 
-var _ dns.Handler = (*GitHandler)(nil)
+func reverse(ss []string) {
+	last := len(ss) - 1
+	for i := 0; i < len(ss)/2; i++ {
+		ss[i], ss[last-i] = ss[last-i], ss[i]
+	}
+}
